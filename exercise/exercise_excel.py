08ed36f24d35b17f7e18b5678c1e4e89f62e75a4
15823596020 <519312875@qq.com>
2020-05-09 20:54:56 +0800

    youhua external data source file processing

--- exercise/exercise_excel.py
+++ exercise/exercise_excel.py
@@ -1,7 +1,6 @@
 from openpyxl import Workbook, load_workbook
 from openpyxl.styles import Alignment
 
-
 class ExerciseExcel:
     def __init__(self, excel_title, excel_data):  # 属性有表头信息，数据信息（通过传入的参数得到）
         self.excel_title = excel_title
@@ -17,14 +16,13 @@ class ExerciseExcel:
             sheet.cell(row=1, column=i + 1).value = excel_title[i]  # 向第1行中的对应列中写入表头信息
             sheet.cell(row=1, column=i + 1).alignment = Alignment(horizontal='center', vertical='center')  # 设置内容居中显示
         # 向表中写入数据
-        for i in range(len(excel_data)):  # 循环行
+        for i in range(len(excel_data)):  # 循环行，一行一行的写入内容
             sheet.cell(row=i + 2, column=1).value = excel_data[i]["姓名"]  # 第1列写入某人的姓名
             sheet.cell(row=i + 2, column=2).value = excel_data[i]["身高"]  # 第2列写入某人的身高
             sheet.cell(row=i + 2, column=3).value = excel_data[i]["体重"]  # 第3列写入某人的体重
             # 设置内容居中显示
-            sheet.cell(row=i + 2, column=1).alignment = Alignment(horizontal='center', vertical='center')
-            sheet.cell(row=i + 2, column=2).alignment = Alignment(horizontal='center', vertical='center')
-            sheet.cell(row=i + 2, column=3).alignment = Alignment(horizontal='center', vertical='center')
+            for j in range(len(excel_data[i])):  # 循环列，某行某列的内容居中显示
+                sheet.cell(row=i + 2, column=j + 1).alignment = Alignment(horizontal='center', vertical='center')
 
         wt.save("./exercise_data/examiner_information.xlsx")  # 保存excel文件
 
@@ -43,18 +41,18 @@ class ExerciseExcel:
 
             if weight == (height - 70) * 0.6:  # 如果体重=公式计算出的体重，则为健康体
                 print(f"{name}:健康")
-                sheet_read.cell(row=i + 2, column=4, value="健康").alignment = Alignment(vertical='center')  # 设置内容垂直居中显示
+                sheet_read.cell(row=i + 2, column=4, value="健康").alignment = Alignment(
+                    vertical='center')  # 写入相关信息，并设置内容垂直居中显示
             elif weight < (height - 70) * 0.6:  # 如果体重<公式计算出的体重，则为偏瘦
                 print(f"{name}:偏瘦,健康体重为{(height - 70) * 0.6}")
                 sheet_read.cell(row=i + 2, column=4, value=f"偏瘦,健康体重为{(height - 70) * 0.6}").alignment = Alignment(
-                    vertical='center')  # 设置内容垂直居中显示
+                    vertical='center')  # 写入相关信息，并设置内容垂直居中显示
             else:  # 如果体>公式计算出的体重，则为偏胖
                 print(f"{name}:偏胖,健康体重为{(height - 70) * 0.6}")
                 sheet_read.cell(row=i + 2, column=4, value=f"偏胖,健康体重为{(height - 70) * 0.6}").alignment = Alignment(
-                    vertical='center')  # 设置内容垂直居中显示
+                    vertical='center')  # 写入相关信息，并设置内容垂直居中显示
 
         rt.save("homework_excel.xlsx")  # 保存excel文件
-
 
 if __name__ == '__main__':
     # 需要传入的表头列表
